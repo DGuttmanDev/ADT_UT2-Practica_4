f71d8251987d1747ec4471152c14b68d120a269b
Daniel Guttman <dguttmandevelop@gmail.com>
2022-11-26 13:42:03 +0100

    Probando a ordenar

--- src/main/java/es/adt/business/service/FemaleCharacterService.java
+++ src/main/java/es/adt/business/service/FemaleCharacterService.java
@@ -1,4 +1,34 @@
 package es.adt.business.service;
 
+import es.adt.business.model.Character;
+import es.adt.business.repository.HttpRequest;
+
+import java.net.MalformedURLException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class FemaleCharacterService {
+
+    public Character getCharactersWithMaxEpisodes() throws MalformedURLException {
+
+        List<Character> characters = HttpRequest.getCharacters();
+
+        Collections.sort(characters, (o1, o2) -> {
+            if(o2.getEpisode().size() > o1.getEpisode().size()){
+                return 1;
+            } else if(o2.getEpisode().size() > o1.getEpisode().size()){
+                return 0;
+            } else {
+                return -1;
+            }
+        });
+
+        System.out.println(characters);
+
+        return null;
+    }
+
+
 }
