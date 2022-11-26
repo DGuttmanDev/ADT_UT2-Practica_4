0ffc6cf19f546d1c2e63a922bda7c3641cdc7c70
Daniel Guttman <dguttmandevelop@gmail.com>
2022-11-26 11:56:15 +0100

    Agregadas dependencias a pom y modificado main
    Se ha eliminado Hola mundo de main

--- src/main/java/es/adt/Main.java
+++ src/main/java/es/adt/Main.java
@@ -1,7 +1,16 @@
 package es.adt;
 
+import es.adt.business.repository.HttpRequest;
+import es.adt.business.service.AlienService;
+
+import java.net.MalformedURLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws MalformedURLException {
+        
+        AlienService as = new AlienService();
+
+        System.out.println(as.getAliveAliens());
+
     }
 }
